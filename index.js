26b683518d2b3025c60fc5dbe5d9c80c417d840a
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-08-16 20:09:52 -0700

    Setup entry point

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// const AWS = require('aws-sdk')
-// const Process = require('./lib/process')
-// const Config = require('./lib/config')
-// const Db = require('./lib/mongo')
-// const Dynamo = require('./lib/dynamo')
+const AWS = require('aws-sdk')
+const Process = require('./lib/process')
+const Config = require('./lib/config')
+const Db = require('./lib/mongo')
+const Dynamo = require('./lib/dynamo')
 
-// const kms = new AWS.KMS({ region: 'us-west-2' })
-// const docs = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' })
+const kms = new AWS.KMS({ region: 'us-west-2' })
+const docs = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' })
 
 /*
 - Get organization info from SQS event
@@ -21,22 +21,22 @@
 - Write computed donation for each package to their respective donationRevenue log
 */
 exports.handler = async (event) => {
-  // const log = console
-  // const dynamo = new Dynamo({ docs })
-  // const config = new Config({ kms })
-  // const db = new Db({ config })
-  // await db.connect()
+  const log = console
+  const dynamo = new Dynamo({ docs })
+  const config = new Config({ kms })
+  const db = new Db({ config })
+  await db.connect()
 
-  // let results
+  let results
   try {
-    // results = await Promise.all(
-    //   event.Records.map(record => Process.process({ record, db, dynamo, log }))
-    // )
-    // if (!results.every(result => result.success)) {
-    //   throw new Error(JSON.stringify(results))
-    // }
-    // return results
+    results = await Promise.all(
+      event.Records.map(record => Process.process({ record, db, dynamo, log }))
+    )
+    if (!results.every(result => result.success)) {
+      throw new Error(JSON.stringify(results))
+    }
+    return results
   } finally {
-    // await db.close()
+    await db.close()
   }
 }
